58861e359d16e3d32b562bac1e6a90c8f0b53e61
memozzeyman <78563776+memozzeyman@users.noreply.github.com>
2021-04-25 00:18:30 +0100

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 CORS(app, support_credentials=True)
 
 # load the learner
-learn = load_learner(path='.', file='eye_recycle_trained_modelfinal.pkl')
+learn = load_learner(path='.', file='modelcmon.pkl')
 classes = learn.data.classes
 
  
